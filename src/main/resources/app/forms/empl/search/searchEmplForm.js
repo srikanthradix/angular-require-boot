9ac7293e9b26ef09ab467475229348f53deb1dc5
srikanthradix <srikanth.vuppula@gmail.com>
2015-10-28 00:05:43 -0500

    show angular data using react doms and get the data back to angular on events

--- src/main/resources/app/forms/empl/search/searchEmplForm.js
+++ src/main/resources/app/forms/empl/search/searchEmplForm.js
@@ -9,97 +9,106 @@
     ], function (angular) {
 
         var React = require('react');
+
         //var ReactDOM = require('reactDom');
-        var app = angular.module('myApp.view2b.searchEmplForm', ['ui.router', 'ngResource', 'angularUtils.directives.dirPagination'])
+        angular.module('myApp.view2b.searchEmplForm', ['ui.router', 'ngResource', 'angularUtils.directives.dirPagination'])
 
-        var REACT_EVENTS = {
-            "REACT_BROADCAST_MSG": "reactBroadcast:msg"
-        };
-
-        var MY_EVENTS = app.constant("MY_EVENTS", REACT_EVENTS);
-
-        //function $broadcast(event: string, ...args: any[]) {
-        //    angular.element('body').injector().invoke([
-        //        '$rootScope',
-        //        '$timeout',
-        //        ($rootScope: ng.IRootScopeService, $timeout: ng.ITimeoutService) =>
-        //    {
-        //        args.unshift(event);
-        //        $timeout(() => {
-        //            $rootScope.$broadcast.apply($rootScope, args);
-        //    });
-        //}
-        //]);
-    //}
-
-        var myEmpReactClass = React.createClass({
-            displayName : 'MyEmployeesList',
-            //propTypes: {
-            //    // A custom object
-            //    employees: React.PropTypes.arrayOf({
-            //        id: React.PropTypes.string,
-            //        name: React.PropTypes.string,
-            //        salary: React.PropTypes.number,
-            //        deptName: React.PropTypes.string
-            //    })
-            //},
-            getDefaultProps: function() {
-                return {
-                    styles: {
-                        table: {
-                            border: "1px solid #dfd7ca",
-                            color: '#3e3f3a',
-                            background: '#b3d4fc',
-                            'borderBottomWidth': '2px',
-                            'borderCollapse': 'collapse',
-                            'width': '90%'
+            .factory('reactEmpTableRendererFactory', function () {
+                var self = {};
+                self.create = function () {
+                    return React.createClass({
+                        displayName: 'MyEmployeesList',
+                        //propTypes: {
+                        //    // A custom object
+                        //    employees: React.PropTypes.arrayOf({
+                        //        id: React.PropTypes.string,
+                        //        name: React.PropTypes.string,
+                        //        salary: React.PropTypes.number,
+                        //        deptName: React.PropTypes.string
+                        //    })
+                        //},
+                        getDefaultProps: function () {
+                            var table = {
+                                border: "1px solid #dfd7ca",
+                                color: '#3e3f3a',
+                                background: '#b3d4fc',
+                                'borderBottomWidth': '2px',
+                                'borderCollapse': 'collapse',
+                                'width': '90%'
+                            };
+                            var td = {
+                                border: '1px solid #999',
+                                padding: '8px',
+                                width: '18%',
+                                textAlign: 'center',
+                                verticalAlign: 'middle'
+                            };
+                            var cols = ['Id', 'Name', 'Salary', 'Department'];
+                            return {
+                                styles: {
+                                    table: table,
+                                    td: td
+                                },
+                                table: {
+                                    cols: cols
+                                }
+                            }
                         },
-                        td: {
-                            border: '1px solid #999',
-                            padding: '8px',
-                            'width': '18%'
+                        render: function () {
+                            var self = this;
+                            var employees = self.props.employees;
+                            var scope = self.props.scope;
+                            var styles = self.props.styles;
+                            var cols = self.props.table.cols;
+
+                            var rows = employees.map(function (emp, idx) {
+
+                                var clickHandler = function (selected, self) {
+                                    emp.selected = selected;
+                                    this.setState({});
+                                    scope.clickHandler.call(scope, emp)
+                                }
+
+                                return (
+                                    React.DOM.tr({key: idx},
+                                        React.DOM.td({style: styles.td}, React.DOM.input({
+                                            type: 'checkbox',
+                                            checked: emp.selected,
+                                            onChange: clickHandler.bind(self, !emp.selected)
+                                        })),
+                                        React.DOM.td({style: styles.td}, emp.id),
+                                        React.DOM.td({style: styles.td}, emp.name),
+                                        React.DOM.td({style: styles.td}, emp.salary),
+                                        React.DOM.td({style: styles.td}, emp.department.name)
+                                    )
+                                );
+                            });
+
+                            //var thead = React.DOM.thead({},
+                            //    React.DOM.tr({},
+                            //        cols.map(function (col, idx) {
+                            //            return React.DOM.th({key: idx}, col);
+                            //        })));
+
+                            var tbody = React.DOM.tbody({}, rows);
+
+                            return (
+                                React.DOM.table(
+                                    {style: styles.table}, tbody
+                                    //[thead, tbody]
+                                )
+                            );
                         }
-                    }
+                    });
                 }
-            },
-            render: function () {
-                var employees = this.props.employees;
-                var scope = this.props.scope;
-                var styles = this.props.styles;
-
-                var rows = employees.map(function (emp, idx) {
-
-                    var clickHandler = function (ev) {
-                        console.log(ev.currentTarget);
-                        scope.$broadcast(REACT_EVENTS.REACT_BROADCAST_MSG, {
-                            msg: ev.currentTarget
-                        });
-                    };
-
-                    return (
-                        React.DOM.tr({key: idx, onClick: clickHandler},
-                            React.DOM.td({style: styles.td}, emp.id),
-                            React.DOM.td({style: styles.td}, emp.name),
-                            React.DOM.td({style: styles.td}, emp.salary),
-                            React.DOM.td({style: styles.td}, emp.department.name)
-                        )
-                    );
-                });
-
-                return (
-                    React.DOM.table({style: styles.table},
-                        React.DOM.tbody(null,rows))
-                );
-            }
-        });
-
-
+                return self;
+            })
 
             //angular.module('myApp')
             //    .getControllerProvider()
             //    .register
-        app.controller('searchEmplCtrl', ['$state', 'srchFormService', 'MY_EVENTS',
-                function ($state, srchFormService, MY_EVENTS) {
+            .controller('searchEmplCtrl', ['$state', 'srchFormService',
+                function ($state, srchFormService) {
                     var self = this;
                     self.emps = [];
                     self.itemsPerPage = srchFormService.getItemsPerPage();
@@ -113,8 +122,8 @@
                     self.navigate = function (route) {
                         $state.go(route);
                     }
-                    self.removeEmployee = function (emp) {
-                        self.emps = srchFormService.removeEmployee(emp);
+                    self.removeEmployee = function () {
+                        self.emps = srchFormService.removeEmployee();
                     }
                     self.order = function (predicate) {
                         srchFormService.order(predicate);
@@ -134,41 +143,45 @@
 
             //angular.module('myApp')
             //    .getCompileProvider()
-        app.directive('displayWithReact', ['$rootScope', 'MY_EVENTS', function ($rootScope, MY_EVENTS) {
-                var self = this || {};
+            .directive('displayWithReact', ['reactEmpTableRendererFactory', 'srchFormService',
+                function (reactEmpTableRendererFactory, srchFormService) {
+                    var self = {};
 
-                self.link = function (scope, elem, attrs, reactCtrl) {
-                    scope.$watchCollection(angular.bind(reactCtrl, function () {
-                        return reactCtrl.employees;
-                    }), function (newVal, oldVal) {
-                        if (newVal === oldVal) {
-                            return;
-                        } // AKA first run
-                        var element = React.createElement(myEmpReactClass, {employees: newVal, scope: scope});
-                        React.render(element, elem[0]);
-                    })
+                    self.link = function (scope, elem, attrs, reactCtrl) {
+                        scope.$watchCollection(angular.bind(reactCtrl, function () {
+                            return reactCtrl.employees;
+                        }), function (newVal, oldVal) {
+                            if (newVal === oldVal) {
+                                return;
+                            } // AKA first run
+                            var element = React.createElement(
+                                reactEmpTableRendererFactory.create(),
+                                {employees: newVal, scope: scope}
+                            );
+                            React.render(element, elem[0]);
+                        })
 
-                    scope.$on(MY_EVENTS.REACT_BROADCAST_MSG, function (event, reactEvent) {
-                        console.log('in Angular');
-                        console.log(reactEvent.msg);
-                    });
-                }
+                        scope.clickHandler = function (emp) {
+                            //console.log('in angular');
+                            //console.log(emp);
+                        }
+                    }
 
-                return {
-                    restrict: 'E',
-                    scope: {},
-                    controller: function () {
-                    },
-                    controllerAs: 'reactCtrl',
-                    bindToController: {
-                        employees: '='
-                    },
-                    link: self.link
-                }
-            }])
+                    return {
+                        restrict: 'E',
+                        scope: {},
+                        controller: function () {
+                        },
+                        controllerAs: 'reactCtrl',
+                        bindToController: {
+                            employees: '='
+                        },
+                        link: self.link
+                    }
+                }])
 
             // Inline edit directive
-        app.directive('editableField', function () {
+            .directive('editableField', function () {
                 var self = this || {};
                 self.link = function (scope, elm, attr, editCtrl) {
                     var previousValue;
@@ -220,7 +233,7 @@
 
             //angular.module('myApp')
             //    .getProvider()
-        app.service('srchFormService', ['$q', 'deptService', function ($q, deptService) {
+            .service('srchFormService', ['$q', 'deptService', function ($q, deptService) {
                 var self = this;
                 self.master = {};
                 self.itemsPerPage = 5;
@@ -251,6 +264,7 @@
                             .$promise.then(function (employees) {
                                 if (employees) {
                                     angular.forEach(employees, function (employee) {
+                                        employee.selected = false;
                                         self.emps.push(employee);
                                     });
                                 }
@@ -268,9 +282,12 @@
                     return self.emps;
                 }
 
-                self.removeEmployee = function (emp) {
-                    var index = self.emps.indexOf(emp);
-                    self.emps.splice(index, 1);
+                self.removeEmployee = function () {
+                    angular.forEach(self.emps, function(emp, idx) {
+                        if(emp.selected === true) {
+                            self.emps.splice(idx, 1);
+                        }
+                    })
                     return self.emps;
                 }
 
@@ -294,19 +311,19 @@
                 }
 
                 self.download = function () {
-                    var data = [];
+                    var data = ["EMP_ID, EMP_NAME, EMP_SALARY, EMP_DEPT" + '\r\n'];
                     angular.forEach(self.emps, function (emp) {
                         var row = emp.id + ",";
                         row = row + emp.name + ",";
                         row = row + emp.salary + ",";
-                        row = row + emp.department.name + ",";
+                        row = row + emp.department.name + '\r\n';
                         data.push(row);
                     });
                     var anchor = angular.element('<a/>');
                     anchor.attr({
                         href: 'data:attachment/csv;charset=utf-8,' + encodeURI(data),
                         target: '_blank',
-                        download: 'filename.csv'
+                        download: 'employees.csv'
                     })[0].click();
                 };
 
