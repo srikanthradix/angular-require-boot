3a233985c3e185a6115cbb6b0a5692727f79294c
srikanthradix <srikanth.vuppula@gmail.com>
2015-11-01 23:28:49 -0600

    added grunt tasks to create minified javascript files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         jshint: {
-            files: ['Gruntfile.js', 'src/main/resources/app/js/**/*.js'],
+            files: ['Gruntfile.js',
+                'src/main/resources/app/js/**/*.js',
+                'src/main/resources/app/config/**/*.js',
+                'src/main/resources/app/app.js'],
             options: {
                 "asi": true,
                 "expr": true
@@ -16,18 +19,51 @@ module.exports = function (grunt) {
             dist: {
                 src: ['src/main/resources/app/js/**/*.js',
                     'src/main/resources/app/config/*.js',
-                    'src/main/resources/app/app/js'],
+                    'src/main/resources/app/app.js'],
                 dest: 'dist/<%= pkg.name %>.js'
             }
         },
         uglify: {
-            options: {
-                // the banner is inserted at the top of the output
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
             dist: {
+                files: grunt.file.expandMapping([
+                        'src/main/resources/app/js/**/*.js',
+                        'src/main/resources/app/config/**/*.js',
+                        'src/main/resources/app/app.js'
+                    ],
+                    '', {
+                        rename: function (destBase, destPath) {
+                            return destBase + destPath.replace('.js', '.min.js');
+                        }
+                    })
+            }
+        },
+        clean: {
+            remove: {
+                src: [
+                    'src/main/resources/app/js/**/*.min.js',
+                    'src/main/resources/app/config/**/*.min.js',
+                    'src/main/resources/app/app.min.js'
+                ]
+            }
+        },
+        'string-replace': {
+            inline: {
                 files: {
-                    'src/main/resources/app/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    './': [
+                        'src/main/resources/app/index.html',
+                        'src/main/resources/app/html/**/*.html',
+                        'src/main/resources/app/js/**/*.js',
+                        'src/main/resources/app/config/**/*.js',
+                        'src/main/resources/app/app.js'
+                    ]
+                },
+                options: {
+                    replacements: [
+                        {
+                            pattern: '.js',
+                            replacement: '.min.js'
+                        }
+                    ]
                 }
             }
         }
@@ -35,8 +71,10 @@ module.exports = function (grunt) {
 
     // load the Grunt task
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-string-replace');
 
-    grunt.registerTask("default", ["concat", "uglify"]);
+    grunt.registerTask("default", ["string-replace", "clean", "uglify"]);
 };
